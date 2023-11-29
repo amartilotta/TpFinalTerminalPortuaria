446062f000a1235588ce97121b4427bd47b6ceee
Agustin Martilotta <agustin.martilotta@hotmail.com>
2023-11-29 20:01:29 -0300

    Nuevos archivos test

---
+++ src/test/java/tpFinalTerminalPortuaria/Terminal/TerminalTestCase.java
@@ -0,0 +1,14 @@
+package tpFinalTerminalPortuaria.Terminal;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TerminalTestCase {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
