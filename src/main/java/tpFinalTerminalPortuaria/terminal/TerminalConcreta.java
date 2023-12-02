fd3845cb9797934ff9711e58c8366e521cfa52f0
Agustin Martilotta <agustin.martilotta@hotmail.com>
2023-12-01 23:12:23 -0300

    Nueva clase

---
+++ src/main/java/tpFinalTerminalPortuaria/terminal/TerminalConcreta.java
@@ -0,0 +1,12 @@
+package tpFinalTerminalPortuaria.terminal;
+
+import tpFinalTerminalPortuaria.Ubicacion.Ubicacion;
+
+public class TerminalConcreta extends Terminal{
+
+	public TerminalConcreta(Ubicacion ubicacion) {
+		super(ubicacion);
+
+	}
+
+}
