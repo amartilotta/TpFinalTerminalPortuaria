95fdf4de3c4defcff2f72e28a3f2236b7ddfa040
Agustin Martilotta <agustin.martilotta@hotmail.com>
2023-12-04 00:52:47 -0300

    actualizacion filtrosPuertoDestino

--- src/test/java/tpFinalTerminalPortuaria/filtrosPuertoDestino/FiltroPuertoDestinoIgualQueTestCase.java
+++ src/test/java/tpFinalTerminalPortuaria/filtrosPuertoDestino/FiltroPuertoDestinoIgualQueTestCase.java
@@ -11,60 +11,58 @@ import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 
-import tpFinalTerminalPortuaria.CircuitoMaritimo.CircuitoMaritimo;
 import tpFinalTerminalPortuaria.Filtros.FiltroPuertoDestinoIgualQue;
+import tpFinalTerminalPortuaria.Viaje.Viaje;
 import tpFinalTerminalPortuaria.terminal.Terminal;
 
 public class FiltroPuertoDestinoIgualQueTestCase {
-	private CircuitoMaritimo circuitoA;
-	private CircuitoMaritimo circuitoB;
-	private List<CircuitoMaritimo> circuitos;
-	private List<CircuitoMaritimo> circuitosB;
-	private FiltroPuertoDestinoIgualQue filtro;
-	private Terminal terminalMock;
-	private Terminal terminalMock2;
-	@Before
-	public void SetUp() {
-		circuitoA = mock(CircuitoMaritimo.class);
-		circuitoB = mock(CircuitoMaritimo.class);
-		circuitos = new ArrayList<CircuitoMaritimo>();
-		circuitosB = new ArrayList<CircuitoMaritimo>();
-		terminalMock  = mock(Terminal.class);
-		terminalMock2 = mock(Terminal.class);
-		filtro = new FiltroPuertoDestinoIgualQue(terminalMock);
-	}
-	
-	@Test
-	public void cuandoCreoFiltroPuertoDestinoIgualQue_EsteTieneUnaTerminal() {
-		assertEquals(filtro.getDestino(), terminalMock);
-	}
-	
-	@Test
-	public void cuandoAUnFiltroPuertoDestinoIgualQue_LePidoFiltrarUnaListaDeCircuitosVacia_EntoncesDevuelveUnaListaVacía() {
-	//	assertTrue(filtro.filtrar(circuitos).size() == 0);
-	}
-	
-	/*@Test
-	public void cuandoFiltroConCircuitosQueNoTienenPuertoDestinoIgualQueElPuertoDelFiltro_EntoncesDevuelveListaVacia() {
-		when(circuitoA.getPuertoDestino()).thenReturn(terminalMock2);
-		when(circuitoB.getPuertoDestino()).thenReturn(terminalMock2);
-		circuitos.add(circuitoA);
-		circuitos.add(circuitoB);
-		List<CircuitoMaritimo> filtrados = filtro.filtrar(circuitos);
-		assertTrue(filtrados.size() == 0);
-	}
-	
-	@Test
-	public void cuandoFiltroConCircuitosQueUnoTienenPuertoDestinoIgualQueElPuertoDelFiltro_EntoncesDevuelveListaConUnElemento() {
-		when(circuitoA.getPuertoDestino()).thenReturn(terminalMock2);
-		when(circuitoB.getPuertoDestino()).thenReturn(terminalMock);
-		circuitos.add(circuitoA);
-		circuitos.add(circuitoB);
-		List<CircuitoMaritimo> filtrados = filtro.filtrar(circuitos);
-		assertTrue(filtrados.size() == 1);
-	}
-	*/
-	
+    private Viaje viajeA;
+    private Viaje viajeB;
+    private List<Viaje> viajes;
+    private FiltroPuertoDestinoIgualQue filtro;
+    private Terminal terminalMock;
+
+    @Before
+    public void SetUp() {
+        viajeA = mock(Viaje.class);
+        viajeB = mock(Viaje.class);
+        viajes = new ArrayList<Viaje>();
+        terminalMock = mock(Terminal.class);
+        filtro = new FiltroPuertoDestinoIgualQue(terminalMock);
+    }
+
+    @Test
+    public void cuandoCreoFiltroPuertoDestinoDistintoQue_EsteTieneUnaTerminal() {
+        assertEquals(filtro.getDestino(), terminalMock);
+    }
+
+    @Test
+    public void cuandoAUnFiltroPuertoDestinoIgualQue_LePidoFiltrarUnaListaDeViajesVacia_EntoncesDevuelveUnaListaVacía() {
+        assertTrue(filtro.filtrar(viajes).size() == 0);
+    }
+
+
+    @Test
+    public void cuandoFiltroConViajesQueNoTienenPuertoDestinoIgualQueElPuertoDelFiltro_EntoncesDevuelveListaVacia() {
+        when(viajeA.tieneTerminal(terminalMock)).thenReturn(false);
+        when(viajeB.tieneTerminal(terminalMock)).thenReturn(false);
+        viajes.add(viajeA);
+        viajes.add(viajeB);
+        List<Viaje> filtrados = filtro.filtrar(viajes);
+        assertTrue(filtrados.size() == 0);
+    }
+
+    @Test
+    public void cuandoFiltroConViajesQueUnoTienenPuertoDestinoIgualQueElPuertoDelFiltro_EntoncesDevuelveListaConUnElemento() {
+        when(viajeA.tieneTerminal(terminalMock)).thenReturn(true);
+        when(viajeB.tieneTerminal(terminalMock)).thenReturn(false);
+        viajes.add(viajeA);
+        viajes.add(viajeB);
+        List<Viaje> filtrados = filtro.filtrar(viajes);
+        assertTrue(filtrados.size() == 1);
+    }
+
+
 }
 
 
